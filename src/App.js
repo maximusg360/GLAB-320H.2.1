fe17aa822cc9acc3f5d1d5dc524c1dcbe496a3da
Guzman <francisco.guzman@accenture.com>
2024-09-18 22:29:28 -0400

    Simple React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content() {
+  return (<p>This is my first React Application, wish me luck!</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Me, of course.</h1>);
+}
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
